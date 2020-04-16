94c76aa3ca2ffae76a1d9fba5f81dde31dea55d4
jrodriquez <jrodri30@students.towson.edu>
2020-04-16 11:42:22 -0400

    ASTR 181 project set-up

---
+++ nasa-api-project/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavBar";
+
+function App() {
+  return (
+
+    
+    <Router>
+    <div className="container">
+      <Navbar/>
+     
+    </div>
+
+  </Router>
+  );
+}
+
+export default App;
---
+++ nasa-api-project/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, {component, Component} from 'react';
+import axios from 'axios'
+
+
+export default class NavBar extends Component {
+    render () {
+        return (
+
+            <nav class="navbar navbar-light bg-light justify-content-between">
+                <a class="navbar-brand">Astronomy 181</a>
+                    <form class="form-inline">
+                      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                </nav>
+        );
+    }
+}
